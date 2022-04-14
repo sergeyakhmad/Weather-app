9dcaeb6321f5d9571b940f4e98d83be1cc496d2c
Sergey Akhmadaliev <Sergan86@ukr.net>
2022-04-14 15:05:37 +0300

    api

--- src/js/api-service.js
+++ src/js/api-service.js
@@ -1,11 +1,14 @@
-const jsondb = require('../config.json');
-const API_KEY = jsondb.API_KEY;
+// const jsondb = require('../config.json');
+// const API_KEY = jsondb.API_KEY;
+const API_KEY = '428ed231089210f0f91b7ff93c680cd1';
 export const today = 'weather';
 export const fiveDays = 'forecast';
 
 export function getWeatherData(city, page = today) {
-    return fetch(`https://api.openweathermap.org/data/2.5/${page}?q=${city}&appid=${API_KEY}`).then(res => {
-        if (!res.ok) return Promise.reject('404');
-        return res.json();
-    })
+  return fetch(`https://api.openweathermap.org/data/2.5/${page}?q=${city}&appid=${API_KEY}`).then(
+    res => {
+      if (!res.ok) return Promise.reject('404');
+      return res.json();
+    },
+  );
 }
