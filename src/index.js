e6b7d4e1956095ea93c4d3ae68b885be8113b88a
s3bull <95282477+s3bull@users.noreply.github.com>
2022-04-14 19:15:08 +0300

    quote(цитата)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './sass/main.scss';
 import './js/event';
 import { fiveDays, getWeatherData, today } from './js/api-service';
+import './js/quote'
 
 getWeatherData('kiev', today).then(console.log);
 getWeatherData('kiev', fiveDays).then(console.log);
---
+++ src/js/quote.js
@@ -0,0 +1,19 @@
+const quoteMarkup = (quote, author) => {
+    document.querySelector('.quote').innerHTML = `
+    <p>${quote}</p>
+    <span>${author}</span>
+    `;
+}
+let arr = [];
+const quote = fetch(`https://api.goprogram.ai/inspiration`).then(response => {
+    if (!response.ok) {
+        throw new Error(response.status);
+    }
+    return response.json();
+})
+    .then(data => {
+        quoteMarkup(data.quote, data.author);
+    })
+    .catch(error => {
+        console.log(error);
+    });;
