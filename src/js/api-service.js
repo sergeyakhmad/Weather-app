9715c0f06c3e94282c68f897ab89a69b93b1ca79
s3bull <95282477+s3bull@users.noreply.github.com>
2022-04-14 14:49:06 +0300

    changes

--- src/js/api-service.js
+++ src/js/api-service.js
@@ -1,11 +1,10 @@
-const BASE_URL = "https://api.openweathermap.org";
 const jsondb = require('../config.json');
 const API_KEY = jsondb.API_KEY;
 export const today = 'weather';
 export const fiveDays = 'forecast';
 
 export function getWeatherData(city, page = today) {
-    return fetch(BASE_URL + '/data/2.5/' + page + `?q=${city}&appid=${API_KEY}`).then(res => {
+    return fetch(`https://api.openweathermap.org/data/2.5/${page}?q=${city}&appid=${API_KEY}`).then(res => {
         if (!res.ok) return Promise.reject('404');
         return res.json();
     })
