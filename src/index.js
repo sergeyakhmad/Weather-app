99e696fe5b6166d760408e2a14c46bf0cc89e571
s3bull <95282477+s3bull@users.noreply.github.com>
2022-04-15 17:26:50 +0300

    bg-img ang quote
    Добавлена функция добавления фоновой картинки, теперь нужно подключить там, где считывается значение инпута. А также пофикшена цитата, которая сломалась в один момент.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import './sass/main.scss';
 import './js/event';
 import { fiveDays, getWeatherData, today } from './js/api-service';
-import './js/quote'
+import './js/quote';
+import './js/bg-api';
 
 getWeatherData('kiev', today).then(console.log);
 getWeatherData('kiev', fiveDays).then(console.log);
