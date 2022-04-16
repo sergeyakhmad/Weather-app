079f6fe8a8c8240923c51c022aea6c7a51e32eb6
Serhii Deineha <sergdd24041997@gmail.com>
2022-04-16 11:14:12 +0300

    five-days section

--- src/js/refs.js
+++ src/js/refs.js
@@ -2,4 +2,8 @@ export default {
   form: document.querySelector('form'),
   favoriteBtn: document.querySelector('#favorites'),
   input: document.querySelector('.thumb-input__text'),
+  homeContainer: document.querySelector('.home-container'),
+  fiveDaysNode: document.querySelector('.five-days-container'),
+  btnDaysNode: document.querySelector('.home-days-btn'),
+  btnContainerNode: document.querySelector('.home-buttons'),
 };
