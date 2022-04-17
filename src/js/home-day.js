5fa8ada3081e7628089bd0ce31de12102415b8ec
OlenaPustova <i.am.elena.p@gmail.com>
2022-04-17 20:39:49 +0300

    Update home-day.js

--- src/js/home-day.js
+++ src/js/home-day.js
@@ -3,33 +3,25 @@ import { fiveDays, getWeatherData, today } from './api-service';
 import refs from './refs/';
 import sprite from '../images/symbol-defs.svg';
 
-getWeatherData('Tokyo', today).then(data => {
+getWeatherData('tokyo', today).then(data => {
   markupHomeDay(data);
 });
 
 function markupHomeDay(data) {
   console.log(data);
   const day = new Date(data.dt * 1000);
+  const timezone = data.timezone * 1000;
 
-  const numberDay = day.getDate();
-  const weekDay = getNameDayHome(day);
+  const currentPlaceTime = convertTimezone(day, timezone);
+
+  const numberDay = currentPlaceTime.getDate();
+  const weekDay = getNameDayHome(currentPlaceTime);
 
   const sunrise = new Date(data.sys.sunrise * 1000);
-
-  console.log(sunrise);
-
-  const currentTimeZoneSR = sunrise.getTimezoneOffset() * 60 * 1000;
-  const dayUTCsr = sunrise.getTime() + currentTimeZoneSR;
-  const timezone = data.timezone * 1000;
-  const currentPlaceSunrise = new Date(dayUTCsr + timezone);
+  const currentPlaceSunrise = convertTimezone(sunrise, timezone);
 
   const sunset = new Date(data.sys.sunset * 1000);
-  const currentTimeZoneSS = sunset.getTimezoneOffset() * 60 * 1000;
-  const dayUTCss = sunset.getTime() + currentTimeZoneSS;
-  // const timezone = data.timezone * 1000;
-  const currentPlaceSunset = new Date(dayUTCss + timezone);
-
-  console.log(sunset.getTime());
+  const currentPlaceSunset = convertTimezone(sunset, timezone);
 
   const sunriseHours = currentPlaceSunrise.getHours();
   const sunriseMinutes = currentPlaceSunrise.getMinutes();
@@ -77,6 +69,7 @@ function getNameDayHome(date) {
 }
 
 function getNameMonthHome(date) {
+  console.log(date);
   const monthNum = date.getMonth();
   let month = '';
   const months = [
@@ -109,22 +102,17 @@ function addLeadingZero(value) {
 function clock(data) {
   setInterval(function () {
     const day = new Date();
-
-    // console.log(day);
-    const currentTimeZone = day.getTimezoneOffset() * 60 * 1000;
-    const dayUTC = day.getTime() + currentTimeZone;
     const timezone = data.timezone * 1000;
-    const currentPlaceTime = new Date(dayUTC + timezone);
+    const currentPlaceTime = convertTimezone(day, timezone);
 
     const clock = document.querySelector('.home-time');
     clock.innerHTML = currentPlaceTime.toLocaleTimeString();
   }, 1000);
 }
 
-function convertTimezone(time) {
+function convertTimezone(time, timezone) {
   const currentTimeZoneSR = time.getTimezoneOffset() * 60 * 1000;
   const dayUTCsr = time.getTime() + currentTimeZoneSR;
-  const timezone = data.timezone * 1000;
-  const currentPlace = new Date(dayUTCsr + timezone);
+  const currentPlaceTime = new Date(dayUTCsr + timezone);
   return currentPlaceTime;
 }
