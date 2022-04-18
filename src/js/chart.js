80ab5fa2b1ba6b3d37e753d11a0bf1055fde2d5f
Oleg Muraviov <95213060+OlegMuraviov@users.noreply.github.com>
2022-04-18 15:10:41 +0300

    chart start JS

--- src/js/chart.js
+++ src/js/chart.js
@@ -9,6 +9,7 @@ Chart.defaults.plugins.legend.position = 'top';
 const labels = ['Date-1', 'Date-2', 'Date-3', 'Date-4', 'Date-5'];
 
 //значения показателей (температура, влажность, скорость ветра, атм осадки)
+const vaulesArr = [0, 1, 0, -1, 0];
 const temperature = [-20, -3, 0, 2, -1];
 const humidity = [-2, -4, 1, 1, 3];
 const windSpeed = [1, 2, 3, 4, 5];
@@ -18,46 +19,14 @@ const dataChart = {
   //даты прогноза
   labels: labels,
 
-  //настройки ярлыков
   datasets: [
     {
-      id: 'temp',
       label: '— Temperature, C° ',
-      data: temperature,
+      data: vaulesArr,
 
       backgroundColor: '#FF6B09',
       borderColor: '#FF6B09',
       cubicInterpolationMode: 'monotone',
-    },
-    {
-      id: 'hum',
-      label: '— Humidity, %',
-      data: humidity,
-
-      backgroundColor: '#0906EB',
-      borderColor: '#0906EB',
-      cubicInterpolationMode: 'monotone',
-      hidden: true,
-    },
-    {
-      id: 'wind',
-      label: '— Wind Speed, m/s',
-      data: windSpeed,
-
-      backgroundColor: '#EA9A05',
-      borderColor: '#EA9A05',
-      cubicInterpolationMode: 'monotone',
-      hidden: true,
-    },
-    {
-      id: 'atmPress',
-      label: '— Atmosphere Pressure, m/m',
-      data: atmPress,
-
-      backgroundColor: '#067806',
-      borderColor: '#067806',
-      cubicInterpolationMode: 'monotone',
-      hidden: true,
     },
   ],
 };
@@ -84,13 +53,6 @@ const config = {
           color: '#ffffff',
         },
       },
-      // title: {
-      //   display: true,
-      //   align: 'start',
-      //   text: 'AVERAGE:',
-      //   color: 'rgba(255, 255, 255, 0.54)',
-      //   position: 'top',
-      // },
       subtitle: {
         display: true,
         text: 'Value of Indicators',
@@ -107,3 +69,68 @@ const config = {
 };
 
 const myChart = new Chart(document.getElementById('myChart'), config);
+
+export function onChartOption(id) {
+  console.log(id);
+  switch (id) {
+    case 'temperature':
+      console.log('temp');
+      dataChart.datasets = [
+        {
+          label: '— Temperature, C° ',
+          data: temperature,
+
+          backgroundColor: '#FF6B09',
+          borderColor: '#FF6B09',
+          cubicInterpolationMode: 'monotone',
+        },
+      ];
+      break;
+
+    case 'humidity':
+      console.log('hum');
+      dataChart.datasets = [
+        {
+          label: '— Humidity, %',
+          data: humidity,
+
+          backgroundColor: '#0906EB',
+          borderColor: '#0906EB',
+          cubicInterpolationMode: 'monotone',
+        },
+      ];
+      break;
+
+    case 'wind':
+      console.log('www');
+      dataChart.datasets = [
+        {
+          id: 'wind',
+          label: '— Wind Speed, m/s',
+          data: windSpeed,
+
+          backgroundColor: '#EA9A05',
+          borderColor: '#EA9A05',
+          cubicInterpolationMode: 'monotone',
+        },
+      ];
+      break;
+
+    case 'atmospherePressure':
+      console.log('444');
+      dataChart.datasets = [
+        {
+          label: '— Atmosphere Pressure, m/m',
+          data: atmPress,
+
+          backgroundColor: '#067806',
+          borderColor: '#067806',
+          cubicInterpolationMode: 'monotone',
+        },
+      ];
+      break;
+
+    default:
+      return;
+  }
+}
