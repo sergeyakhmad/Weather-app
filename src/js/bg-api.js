fc4477b437cae5f592ae80f6c93ad03217b3beac
Serhii Deineha <sergdd24041997@gmail.com>
2022-04-18 14:54:08 +0300

    update function bg

--- src/js/bg-api.js
+++ src/js/bg-api.js
@@ -2,14 +2,15 @@ function getRandomNum(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
-function img(city) {
-  return fetch(
+async function img(city) {
+  return await fetch(
     `https://api.unsplash.com/search/photos/?client_id=LZvDZOjmJ3YNQFh2j9aXsuN6LDWTFwfPTOC_ddGKNmc&query=${city}&page=1&per_page=10&orientation=landscape`,
   ).then(res => {
     if (!res.ok) return Promise.reject('404');
     return res.json();
   });
 }
+
 export function bgImg(city) {
   if (localStorage.getItem(city))
     document.querySelector(
@@ -19,15 +20,20 @@ export function bgImg(city) {
     })`;
   else
     img(city)
-      .then(res => Object.entries(res))
-      .then(res => res[2])
-      .then(res => res[1])
+      .then(res => {
+        return Object.entries(res);
+      })
+      .then(res => {
+        return res[2];
+      })
+      .then(res => {
+        if (res[1].length) return res[1];
+        return Promise.reject('404');
+      })
       .then(res => res.map(elem => elem.urls.regular))
-      // .then(console.log)
       .then(data => {
         document.querySelector('body').style.backgroundImage = `url(${data[getRandomNum(1, 10)]})`;
         localStorage.setItem(city, data);
-      });
+      })
+      .catch(err => console.log('NE OK'));
 }
-
-bgImg('kyiv');
