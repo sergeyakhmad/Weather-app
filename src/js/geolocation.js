5f6bf6c38794a977074cebb0c6b96ad510315729
s3bull <95282477+s3bull@users.noreply.github.com>
2022-04-18 15:33:22 +0300

    Location
    Дубль 2

--- src/js/geolocation.js
+++ src/js/geolocation.js
@@ -1,18 +1,18 @@
 import refs from './refs'
 
-document.querySelector('#search').addEventListener('click', () => navigator.geolocation.getCurrentPosition(success, defaultData))
-
-function defaultData() {
-    defaultReqWeather('Kyiv');
-}
-
-async function success(position) {
-    const coordinates = position.coords;
-
-    return await fetch(`https://api.opencagedata.com/geocode/v1/json?q=q=${coordinates.latitude}+${coordinates.longitude}&key=b2768f8fbe934cd7b99d37f5117e7ccd&language=en`)
+async function success(lat, lon) {
+    return await fetch(`https://api.opencagedata.com/geocode/v1/json?q=${lat}+${lon}&key=b2768f8fbe934cd7b99d37f5117e7ccd&language=en`)
         .then(res => res.json())
         .then(Object.entries)
         .then(res => res[3])
         .then(res => res[1])
-        .then(res => refs.input.value = res[0].formatted.split(',')[1]);
+        .then(res => refs.input.value = res[0].formatted.split(',')[3]);
 }
+
+document.querySelector('#search').addEventListener('click', () => {
+    navigator.geolocation.getCurrentPosition(function (position) {
+        const lat = position.coords.latitude;
+        const lon = position.coords.longitude;
+        success(lat, lon);
+    });
+});
